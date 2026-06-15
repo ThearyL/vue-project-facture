f63edea58645ee4ccee6804cd6a49a838cdec3b6
ThearyL <142918338+ThearyL@users.noreply.github.com>
2026-06-15 18:07:53 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/vue-project-facture/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
